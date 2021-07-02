2041e64e45d94d51c44b7f7e7620d9993a4a6ec3
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-02 19:17:31 +0800

    added logged_price in transaction_logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_044646) do
+ActiveRecord::Schema.define(version: 2021_07_02_110200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_044646) do
     t.integer "amount_change"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "logged_price"
     t.index ["transaction_id"], name: "index_transaction_logs_on_transaction_id"
   end
 
