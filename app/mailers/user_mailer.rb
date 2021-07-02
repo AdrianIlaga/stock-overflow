f8dd2d964a1cb4b5d2924ba0bfc999b64ca9225b
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-02 17:36:51 +0800

    added approval email for brokers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,12 @@ class UserMailer < ApplicationMailer
     @user = user
     @account_type = @user.user_type.user_type
     return unless %w[buyer broker].include?(@account_type)
+
     mail(to: @user.email, subject: 'Welcome to Stock Overflow')
+  end
+
+  def approval_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Broker Account Approval')
   end
 end
