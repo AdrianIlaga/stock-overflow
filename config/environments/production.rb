88a7f025624a3f2d0cb8c789488b4ed71c89e853
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-03 09:56:15 +0800

    changed prod delivery method to letter opener web

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://stock-overflow-al.herokuapp.com/'}
 
-  config.action_mailer.delivery_method = :letter_opener
-config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 end
